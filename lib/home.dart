435febe1152f8348d692e300c23a4772036f3c18
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-16 14:39:46 +0100

    added

--- lib/home.dart
+++ lib/home.dart
@@ -44,44 +44,49 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Stack(
-              children: [
-                SfCircularChart(
-                 // title: const ChartTitle(text: 'World Bank Data Overview for Nigeria'),
-                    series: <CircularSeries>[
-                      // Renders radial bar chart
-                      RadialBarSeries<Map<String, dynamic>, String>(
-                          dataSource: chartData,
-                          xValueMapper: (Map<String, dynamic> data, _) => data['x'],
-                          yValueMapper: (Map<String, dynamic> data, _) => data['y'],
-                          dataLabelSettings: DataLabelSettings(
-                            isVisible: true,
-                            labelPosition: ChartDataLabelPosition.inside,
-                            //labelAlignment: ChartDataLabelAlignment.outer,
-                              textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.pink),
-                              builder: (context, point, series, pointIdex, seriesIndex) {
-                              final xValue = point.x;
-                              final yValue = point.y;
-                              return Text('$xValue: $yValue');
-                              },
-                          ),
-                      )
-                    ]
-                ),
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: Colors.black)
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Stack(
+                children: [
+                  SfCircularChart(
+                    title: const ChartTitle(
+                        text: 'World Bank Data Overview for Nigeria',
                     ),
-                    child: ClipOval(
-                      child: Image.asset('assets/images/img3.png'),
-                    ),
+                      series: <CircularSeries>[
+                        // Renders radial bar chart
+                        RadialBarSeries<Map<String, dynamic>, String>(
+                            dataSource: chartData,
+                            xValueMapper: (Map<String, dynamic> data, _) => data['x'],
+                            yValueMapper: (Map<String, dynamic> data, _) => data['y'],
+                            dataLabelSettings: DataLabelSettings(
+                              isVisible: true,
+                              labelPosition: ChartDataLabelPosition.inside,
+                              //labelAlignment: ChartDataLabelAlignment.outer,
+                                textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.pink),
+                                builder: (context, point, series, pointIdex, seriesIndex) {
+                                final xValue = point.x;
+                                final yValue = point.y;
+                                return Text('$xValue: $yValue');
+                                },
+                            ),
+                        )
+                      ]
                   ),
-                )
-              ],
+                  Center(
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black)
+                      ),
+                      child: ClipOval(
+                        child: Image.asset('assets/images/img3.png'),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
         )
     );
