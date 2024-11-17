6152835a4bc89ab977ef7369b9160c1f69889591
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-17 20:24:41 +0100

    added

--- lib/home.dart
+++ lib/home.dart
@@ -38,7 +38,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       }
     });
   }
-
+//FETCH DATA FROM API
   Future<void> fetchData() async {
     try {
       final fetchData = await  fetchAPI();
